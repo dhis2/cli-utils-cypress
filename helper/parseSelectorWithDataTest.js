d030419f9424702efd52f999f6842c9b1bc6489e
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-05-27 09:11:19 +0200

    fix: remove require statements that causes support index file to throw an err

--- helper/parseSelectorWithDataTest.js
+++
@@ -1,9 +0,0 @@
-const dataTestNameToSelector = require('./dataTestNameToSelector')
-
-const parseSelectorWithDataTest = (selector, prefix) => {
-    return selector.replace(/\{([^}]*)\}/g, (match, dataTestName) =>
-        dataTestNameToSelector(dataTestName, prefix)
-    )
-}
-
-export default parseSelectorWithDataTest
