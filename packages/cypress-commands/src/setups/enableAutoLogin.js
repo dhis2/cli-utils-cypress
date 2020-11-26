402d5971b6b53b5e4871f8744b82a982524f2ee6
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-11-26 09:28:32 +0100

    fix(enable auto login): preserve JSESSIONID cookie correctly after api update

--- packages/cypress-commands/src/setups/enableAutoLogin.js
+++ packages/cypress-commands/src/setups/enableAutoLogin.js
@@ -1,12 +1,13 @@
 /* globals before */
 export const enableAutoLogin = () => {
     before(() => {
-        // Persist this across tests so we don't have to login before each test
-        Cypress.Cookies.defaults({ whitelist: 'JSESSIONID' })
         cy.login()
     })
 
     beforeEach(() => {
+        // Persist this across tests so we don't have to login before each test
+        Cypress.Cookies.preserveOnce('JSESSIONID')
+
         // This ensures the app platform knows which URL to use even if
         // REACT_APP_DHIS2_BASE_URL is undefined It also ensures that the value
         // from the cypress env is used instead of REACT_APP_DHIS2_BASE_URL
