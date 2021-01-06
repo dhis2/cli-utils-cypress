6c5a36978c04da1dddd0f424294691a1df46a663
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2021-01-06 12:43:38 +0100

    fix(commands): add `-W` to `yarn add` when in yarn workspaces root (#121)
    * fix(commands): add -W to yarn install when in yarn workspaces root
    
    * fix(commands): improve error message when reading package.json fails
    
    * chore(commands): improve install command error message
    
    Co-authored-by: Austin McGee <947888+amcgee@users.noreply.github.com>
    
    Co-authored-by: Austin McGee <947888+amcgee@users.noreply.github.com>

--- packages/cli/src/commands/install/ensureDependency.js
+++ packages/cli/src/commands/install/ensureDependency.js
@@ -1,11 +1,31 @@
+const fs = require('fs')
 const exec = require('@dhis2/cli-helpers-engine').exec
 const log = require('@dhis2/cli-helpers-engine').reporter
 
+const isYarnWorkspacesRoot = () => {
+    try {
+        const rootDir = process.cwd()
+        const packageJson = fs.readFileSync(`${rootDir}/package.json`, {
+            encoding: 'utf8',
+        })
+
+        return 'workspaces' in JSON.parse(packageJson)
+    } catch (_error) {
+        throw new Error(
+            `'d2-utils-cypress install' must be run from the root of an npm package, but no valid 'package.json' was found in '${process.cwd()}'`
+        )
+    }
+}
+
 const installSupportPackage = (packageName, packageManager, cwd) => {
-    const packageManagerArgs =
-        packageManager === 'yarn' ? ['add', '--dev'] : ['install', '-D']
+    const isYarn = packageManager === 'yarn'
+    const packageManagerArgs = isYarn ? ['add', '--dev'] : ['install', '-D']
 
     const args = [...packageManagerArgs, packageName]
+
+    if (isYarn && isYarnWorkspacesRoot()) {
+        args.push('-W')
+    }
 
     return exec({ cmd: packageManager, args, cwd })
 }
