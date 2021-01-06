2c6de929e15a425de5bcf63e92c24e0115af0a7e
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2021-01-06 12:44:29 +0100

    fix(commands): adjust generated code changes (#124)
    * fix(commands): remove code that produces superfluous imports
    
    * fix(commands): add codemod that cleans up plugins during install command
    
    * chore(commands): add comments to clarify codemod
    
    * test(commands): add test for purge-plugin-legacy-code codemod
    
    * test(cypress-commands): refactor codemod test to use official helper
    
    * chore(jest): clean up redundant config
    
    * chore(test): add indentation for readability

--- packages/cypress-commands/codemods/__test__/replace-get-find-with-xxxxGetWithDataTest.test.js
+++ packages/cypress-commands/codemods/__test__/replace-get-find-with-xxxxGetWithDataTest.test.js
@@ -1,43 +1,29 @@
-import assertDirEqual from 'assert-dir-equal'
-import fs from 'fs-extra'
-import path from 'path'
-import spawn from 'cross-spawn'
+const defineInlineTest = require('jscodeshift/dist/testUtils').defineInlineTest
+const transform = require('../replace-get-find-with-xxxxGetWithDataTest.js')
+const transformOptions = {}
 
-/**
- * This test will not work in watch mode.
- * I already tried to add the `actual` folder to the ignored module paths
- * in the jest condig, but it didn't work
- */
-describe('Transform vanilla cypress commands to custom dhis2 commands', () => {
-    const PATH_ALL = path.join(
-        __dirname,
-        'replace-get-find-with-xxxxGetWithDataTest'
-    )
-    const PATH_SOURCE = path.join(PATH_ALL, 'get-find-source')
-    const PATH_ACTUAL = path.join(PATH_ALL, 'get-find-actual')
-    const PATH_EXPECTED = path.join(PATH_ALL, 'get-find-expected')
-    const FILE_CODEMOD = path.join(
-        __dirname,
-        '..',
-        'replace-get-find-with-xxxxGetWithDataTest.js'
-    )
+defineInlineTest(
+    transform,
+    transformOptions,
+    // input
+    `
+// should get modified
+cy.get('{data-test-name}:first-child button')
+cy.get('div').find('{data-test-name}:first-child button')
 
-    beforeEach(() => {
-        // make sure the generated file is not there
-        fs.removeSync(PATH_ACTUAL)
+// should not get modified
+cy.get(':first-child button')
+cy.get('div').find(':first-child button')
+`,
+    // output
+    `
+// should get modified
+cy.getWithDataTest('{data-test-name}:first-child button')
+cy.get('div').findWithDataTest('{data-test-name}:first-child button')
 
-        // add files the test should work on
-        fs.copySync(PATH_SOURCE, PATH_ACTUAL)
-    })
-
-    afterAll(() => {
-        // clean up
-        fs.removeSync(PATH_ACTUAL)
-    })
-
-    it('should transform all "get"/"find" that contain the custom data test selector syntax', () => {
-        // perform the codemod on the "actual" file
-        spawn.sync('jscodeshift', ['-t', FILE_CODEMOD, PATH_ACTUAL])
-        assertDirEqual(PATH_ACTUAL, PATH_EXPECTED)
-    })
-})
+// should not get modified
+cy.get(':first-child button')
+cy.get('div').find(':first-child button')
+`,
+    'should transform all "get"/"find" that contain the custom data test selector syntax'
+)
--- packages/cypress-commands/codemods/__test__/replace-get-find-with-xxxxGetWithDataTest/get-find-source/index.js
+++
@@ -1,7 +0,0 @@
-// should get modified
-cy.get('{data-test-name}:first-child button')
-cy.get('div').find('{data-test-name}:first-child button')
-
-// should not get modified
-cy.get(':first-child button')
-cy.get('div').find(':first-child button')
