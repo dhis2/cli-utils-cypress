07175fe39859738693682e30fe61c3dc66101b82
Jan-Gerke Salomon <Mohammer5@users.noreply.github.com>
2021-02-10 11:58:12 +0100

    fix(replace getfind mod): add checks to prevent crashes (#135)

--- packages/cypress-commands/codemods/replace-get-find-with-xxxxGetWithDataTest.js
+++ packages/cypress-commands/codemods/replace-get-find-with-xxxxGetWithDataTest.js
@@ -1,13 +1,35 @@
 const containsDataTestSyntax = new RegExp('{[^}]+}')
 
-const isGetOrFind = node =>
-    ['get', 'find'].includes(node.value.callee.property.name)
+const isGetOrFind = node => {
+    if (!node.value) return false
+    if (!node.value.callee) return false
+    if (!node.value.callee.property) return false
 
-const callExpressionHasArguments = node => node.value.arguments.length
+    return ['get', 'find'].includes(node.value.callee.property.name)
+}
+
+const callExpressionHasArguments = node => {
+    if (!node.value) return false
+    if (!node.value.arguments) return false
+
+    return node.value.arguments.length
+}
 
 const selectorArgumentContainsDataTestSyntax = node => {
+    if (!node.value) return false
+    if (!node.value.arguments) return false
+
     const selectorArguments = node.value.arguments
-    const [{ value }] = selectorArguments
+
+    if (!Array.isArray(selectorArguments) || !selectorArguments.length)
+        return false
+
+    const [firstItem] = selectorArguments
+
+    if (!firstItem) return false
+
+    const { value } = firstItem
+
     return containsDataTestSyntax.test(value)
 }
 
@@ -20,6 +42,10 @@ module.exports = function (fileInfo, api) {
         .filter(callExpressionHasArguments)
         .filter(selectorArgumentContainsDataTestSyntax)
         .forEach(node => {
+            if (!node.value) return false
+            if (!node.value.callee) return false
+            if (!node.value.callee.property) return false
+
             const { name } = node.value.callee.property
 
             node.value.callee.property.name =
