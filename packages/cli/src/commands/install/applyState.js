dhis2/cli-utils-cypress

commit 2c6de929e15a425de5bcf63e92c24e0115af0a7e

fix(commands): adjust generated code changes (#124)
* fix(commands): remove code that produces superfluous imports

* fix(commands): add codemod that cleans up plugins during install command

* chore(commands): add comments to clarify codemod

* test(commands): add test for purge-plugin-legacy-code codemod

* test(cypress-commands): refactor codemod test to use official helper

* chore(jest): clean up redundant config

* chore(test): add indentation for readability

packages/cli/src/commands/install/applyState.js
@@ -16,8 +16,8 @@ const {
 const { mergeCypressJsonContents } = require('./mergeCypressJsonContents.js')
 const { mergePluginFileImports } = require('./mergePluginFileImports.js')
 const { mergePluginSetupCalls } = require('./mergePluginSetupCalls.js')
-const { mergeSupportFileImports } = require('./mergeSupportFileImports.js')
 const { mergeSupportSetupCalls } = require('./mergeSupportSetupCalls.js')
+const { purgePluginLegacyCode } = require('./purgePluginLegacyCode.js')
 
 module.exports.applyState = async ({ state, packageManager, paths }) => {
     ensureDirectories(paths)
@@ -27,13 +27,13 @@ module.exports.applyState = async ({ state, packageManager, paths }) => {
 
     if (state.plugins.length) {
         await ensureDependency('@dhis2/cypress-plugins', packageManager, paths)
+        await purgePluginLegacyCode(paths)
         await mergePluginFileImports(state.plugins, paths)
         await mergePluginSetupCalls(state.plugins, paths)
     }
 
     if (state.support.length) {
         await ensureDependency('@dhis2/cypress-commands', packageManager, paths)
-        await mergeSupportFileImports(state.support, paths)
         await mergeSupportSetupCalls(state.support, paths)
     }
 

packages/cli/src/commands/install/mergeSupportFileImports.js
@@ -1,21 +1,0 @@
-const { runCodemod } = require('../../utils/runCodemod.js')
-const path = require('path')
-
-/**
- * Copied from: https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
- */
-const onlyUnique = (value, index, self) => self.indexOf(value) === index
-
-module.exports.mergeSupportFileImports = (support, paths) => {
-    const importNames = support.filter(onlyUnique)
-
-    const codemod = path.join(
-        paths.CLI_TOOL_ROOT,
-        'codemods',
-        'add-import-statement.js'
-    )
-    const files = [paths.CYPRESS_SUPPORT_FILE_DESTINATION]
-    const options = { importNames, packageName: '@dhis2/cypress-plugins' }
-
-    return runCodemod(codemod, files, options)
-}

.huskyrc.js
@@ -10,5 +10,6 @@ module.exports = {
             'd2-style js check --staged',
             'd2-style text check --staged',
         ]),
+        'pre-push': 'yarn test',
     },
 }

packages/cli/codemods/__tests_/purge-plugin-legacy-code.test.js
@@ -1,0 +1,53 @@
+const defineInlineTest = require('jscodeshift/dist/testUtils').defineInlineTest
+const transform = require('../purge-plugin-legacy-code.js')
+const transformOptions = {}
+
+defineInlineTest(
+    transform,
+    transformOptions,
+    "const plugins = require('@dhis2/cli-utils-cypress/plugins')",
+    '',
+    'Removes the legacy import'
+)
+
+defineInlineTest(
+    transform,
+    transformOptions,
+    'plugins(on, config)',
+    '',
+    'Removes the legacy function call'
+)
+
+defineInlineTest(
+    transform,
+    transformOptions,
+    // input
+    `
+    const {
+        chromeAllowXSiteCookies,
+        cucumberPreprocessor
+    } = require('@dhis2/cypress-plugins');
+
+    const plugins = require('@dhis2/cli-utils-cypress/plugins')
+
+    module.exports = (on, config) => {
+        chromeAllowXSiteCookies(on, config);
+        cucumberPreprocessor(on, config);
+
+        plugins(on, config)
+    }
+    `,
+    // output
+    `
+    const {
+        chromeAllowXSiteCookies,
+        cucumberPreprocessor
+    } = require('@dhis2/cypress-plugins');
+
+    module.exports = (on, config) => {
+        chromeAllowXSiteCookies(on, config);
+        cucumberPreprocessor(on, config);
+    }
+    `,
+    'Correctly transforms a typical file'
+)

packages/cli/codemods/purge-plugin-legacy-code.js
@@ -1,0 +1,34 @@
+module.exports = function purgePluginLegacyCode(fileInfo, api) {
+    try {
+        const j = api.jscodeshift
+        const ast = j(fileInfo.source)
+
+        // Removes the following line:
+        // const plugins = require('@dhis2/cli-utils-cypress/plugins')
+        ast.find(j.CallExpression, path => {
+            const isCallToRequire = path.callee.name === 'require'
+            const hasCorrectArgs =
+                path.arguments.length === 1 &&
+                path.arguments[0].value === '@dhis2/cli-utils-cypress/plugins'
+
+            return isCallToRequire && hasCorrectArgs
+        })
+            .closest(j.VariableDeclaration)
+            .remove()
+
+        // Removes the following line:
+        // plugins(on, config)
+        ast.find(j.CallExpression, path => {
+            const isCallToPlugins = path.callee.name === 'plugins'
+            const hasCorrectArgs =
+                path.arguments.length === 2 &&
+                path.arguments[0].name === 'on' &&
+                path.arguments[1].name === 'config'
+            return isCallToPlugins && hasCorrectArgs
+        }).remove()
+
+        return ast.toSource({ quote: 'single' })
+    } catch (e) {
+        console.error(e)
+    }
+}

packages/cli/src/commands/install/purgePluginLegacyCode.js
@@ -1,0 +1,13 @@
+const path = require('path')
+const { runCodemod } = require('../../utils/runCodemod.js')
+
+module.exports.purgePluginLegacyCode = async function (paths) {
+    const codemod = path.join(
+        paths.CLI_TOOL_ROOT,
+        'codemods',
+        'purge-plugin-legacy-code.js'
+    )
+    const files = [paths.CYPRESS_PLUGIN_TEMPLATE_DESTINATION]
+
+    return runCodemod(codemod, files, {})
+}

packages/cypress-commands/codemods/__test__/.gitignore
@@ -1,1 +1,0 @@
-get-find-actual

packages/cypress-commands/codemods/__test__/replace-get-find-with-xxxxGetWithDataTest.test.js
@@ -1,43 +1,29 @@
-import assertDirEqual from 'assert-dir-equal'
-import fs from 'fs-extra'
-import path from 'path'
-import spawn from 'cross-spawn'
+const defineInlineTest = require('jscodeshift/dist/testUtils').defineInlineTest
+const transform = require('../replace-get-find-with-xxxxGetWithDataTest.js')
+const transformOptions = {}
 
-/**
- * This test will not work in watch mode.
- * I already tried to add the `actual` folder to the ignored module paths
- * in the jest condig, but it didn't work
- */
-describe('Transform vanilla cypress commands to custom dhis2 commands', () => {
-    const PATH_ALL = path.join(
-        __dirname,
-        'replace-get-find-with-xxxxGetWithDataTest'
-    )
-    const PATH_SOURCE = path.join(PATH_ALL, 'get-find-source')
-    const PATH_ACTUAL = path.join(PATH_ALL, 'get-find-actual')
-    const PATH_EXPECTED = path.join(PATH_ALL, 'get-find-expected')
-    const FILE_CODEMOD = path.join(
-        __dirname,
-        '..',
-        'replace-get-find-with-xxxxGetWithDataTest.js'
-    )
+defineInlineTest(
+    transform,
+    transformOptions,
+    // input
+    `
+// should get modified
+cy.get('{data-test-name}:first-child button')
+cy.get('div').find('{data-test-name}:first-child button')
 
-    beforeEach(() => {
-        // make sure the generated file is not there
-        fs.removeSync(PATH_ACTUAL)
+// should not get modified
+cy.get(':first-child button')
+cy.get('div').find(':first-child button')
+`,
+    // output
+    `
+// should get modified
+cy.getWithDataTest('{data-test-name}:first-child button')
+cy.get('div').findWithDataTest('{data-test-name}:first-child button')
 
-        // add files the test should work on
-        fs.copySync(PATH_SOURCE, PATH_ACTUAL)
-    })
-
-    afterAll(() => {
-        // clean up
-        fs.removeSync(PATH_ACTUAL)
-    })
-
-    it('should transform all "get"/"find" that contain the custom data test selector syntax', () => {
-        // perform the codemod on the "actual" file
-        spawn.sync('jscodeshift', ['-t', FILE_CODEMOD, PATH_ACTUAL])
-        assertDirEqual(PATH_ACTUAL, PATH_EXPECTED)
-    })
-})
+// should not get modified
+cy.get(':first-child button')
+cy.get('div').find(':first-child button')
+`,
+    'should transform all "get"/"find" that contain the custom data test selector syntax'
+)

commit a8e3c39f77daf3a56bbaaf4ec7a38d166059d196

fix(install): add imports to support file
Fixes a regression created by
https://github.com/dhis2/cli-utils-cypress/pull/124

packages/cli/src/commands/install/applyState.js
@@ -16,6 +16,7 @@ const {
 const { mergeCypressJsonContents } = require('./mergeCypressJsonContents.js')
 const { mergePluginFileImports } = require('./mergePluginFileImports.js')
 const { mergePluginSetupCalls } = require('./mergePluginSetupCalls.js')
+const { mergeSupportFileImports } = require('./mergeSupportFileImports.js')
 const { mergeSupportSetupCalls } = require('./mergeSupportSetupCalls.js')
 const { purgePluginLegacyCode } = require('./purgePluginLegacyCode.js')
 
@@ -34,6 +35,7 @@ module.exports.applyState = async ({ state, packageManager, paths }) => {
 
     if (state.support.length) {
         await ensureDependency('@dhis2/cypress-commands', packageManager, paths)
+        await mergeSupportFileImports(state.support, paths)
         await mergeSupportSetupCalls(state.support, paths)
     }
 

packages/cli/src/commands/install/mergeSupportFileImports.js
@@ -6,16 +6,16 @@ const { runCodemod } = require('../../utils/runCodemod.js')
  */
 const onlyUnique = (value, index, self) => self.indexOf(value) === index
 
-module.exports.mergePluginFileImports = (plugins, paths) => {
-    const importNames = plugins.filter(onlyUnique)
+module.exports.mergeSupportFileImports = (support, paths) => {
+    const importNames = support.filter(onlyUnique)
 
     const codemod = path.join(
         paths.CLI_TOOL_ROOT,
         'codemods',
-        'add-require-statement.js'
+        'add-import-statement.js'
     )
-    const files = [paths.CYPRESS_PLUGIN_TEMPLATE_DESTINATION]
-    const options = { importNames, packageName: '@dhis2/cypress-plugins' }
+    const files = [paths.CYPRESS_SUPPORT_FILE_DESTINATION]
+    const options = { importNames, packageName: '@dhis2/cypress-commands' }
 
     return runCodemod(codemod, files, options)
 }