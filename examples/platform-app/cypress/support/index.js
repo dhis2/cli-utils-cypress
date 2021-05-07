03ab296d200410bf48a3cf391bd242d8060b94ed
Hendrik de Graaf <hendrik@dhis2.org>
2021-05-07 13:15:07 +0200

    fix: login first then shim the network

--- examples/platform-app/cypress/support/index.js
+++ examples/platform-app/cypress/support/index.js
@@ -1,7 +1,6 @@
 import { enableAutoLogin, enableNetworkShim } from '@dhis2/cypress-commands'
 
-enableNetworkShim()
-
 if (Cypress.env('dhis2_api_stub_mode') !== 'STUB') {
     enableAutoLogin()
 }
+enableNetworkShim()
