c51ab5d6d8d642fa93e8583cb7769146a02160d2
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2021-10-28 12:15:09 +0200

    fix: require module in function scope (#288)

--- packages/cypress-plugins/src/plugins/cucumberPreprocessor.js
+++ packages/cypress-plugins/src/plugins/cucumberPreprocessor.js
@@ -1,5 +1,4 @@
-const cucumber = require('cypress-cucumber-preprocessor').default
-
 module.exports = function cucumberPreprocessor(on) {
+    const cucumber = require('cypress-cucumber-preprocessor').default
     on('file:preprocessor', cucumber())
 }
