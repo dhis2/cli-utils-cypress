f4078d79cfb58e814b0362f9c6fc11c0dee992db
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2023-05-03 15:26:50 +0200

    fix(cucumber plugin): require dependencies inside plugin function

--- packages/cypress-plugins/src/plugins/cucumberPreprocessor.js
+++ packages/cypress-plugins/src/plugins/cucumberPreprocessor.js
@@ -1,7 +1,7 @@
-const preprocessor = require('@badeball/cypress-cucumber-preprocessor')
-const webpack = require('@cypress/webpack-preprocessor')
-
 module.exports = async function cucumberPreprocessor(on, config) {
+    const preprocessor = require('@badeball/cypress-cucumber-preprocessor')
+    const webpack = require('@cypress/webpack-preprocessor')
+
     // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
     await preprocessor.addCucumberPreprocessorPlugin(on, config)
 
