ece308e56938a236d4c61dd74918e735c8a3c505
Hendrik de Graaf <hendrik@dhis2.org>
2026-04-07 12:07:18 +0200

    chore: fix file extension issue

--- examples/testing-network-shim-app/src/json-server/resetDb.js
+++ examples/testing-network-shim-app/src/json-server/resetDb.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 import { resourcePath } from '../resourcePath.js'
-import getInitialData from './db.js'
+import getInitialData from './db.cjs'
 
 export const resetDb = () => axios.post(resourcePath('reset'), getInitialData())
