9b44d9f60e2f388311fedececb0db48c128b8b77
Hendrik de Graaf <hendrik@dhis2.org>
2026-04-07 12:25:21 +0200

    chore: remove strict mode to avoid mount effect to run twice

--- examples/testing-network-shim-app/src/index.jsx
+++ examples/testing-network-shim-app/src/index.jsx
@@ -3,8 +3,4 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+createRoot(document.getElementById('root')).render(<App />)
